76d3b166c4a44a58876b5dfbe5bc68c239221cfd
Kush Patel <113313092+kush-7k@users.noreply.github.com>
2024-04-08 21:45:56 -0400

    dynamic metadata

--- routing-demo/src/app/about/page.tsx
+++ routing-demo/src/app/about/page.tsx
@@ -1,3 +1,7 @@
+export const metadata = {
+    title: " About NextJS coding"
+}
+
 export default function About(){
     return <h1>About Me</h1>
 }
--- routing-demo/src/app/products/[productId]/page.tsx
+++ routing-demo/src/app/products/[productId]/page.tsx
@@ -1,3 +1,21 @@
+import { Metadata } from "next";
+type Props = {
+    params: {
+        productId: string
+    }
+}
+
+//This is the dynamic routing of the generate metadata and also it can be an async funtion too.
+export const generateMetadata = async ({ params }: Props): Promise<Metadata> =>{
+    const title = await new Promise(resolve =>{
+        setTimeout(()=>{
+            resolve(`iPhone ${params.productId}`)
+        },100)
+    })
+    return {
+        title: `Product ${params.productId}`
+    }
+}
 export default function ProductDetails({ params }: {
     params: {productId: string}
 }){
