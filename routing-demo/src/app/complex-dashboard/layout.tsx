83950686bf67fd0a941b4c2c9ab67a04dbadbb60
Kush Patel <113313092+kush-7k@users.noreply.github.com>
2024-04-16 13:40:02 -0400

    intercepting routes

--- routing-demo/src/app/complex-dashboard/layout.tsx
+++ routing-demo/src/app/complex-dashboard/layout.tsx
@@ -12,7 +12,7 @@ export default function DashboardLayout({
     notifications: React.ReactNode
     children: React.ReactNode;
 }){
-    const isLoggedIn = false;
+    const isLoggedIn = true;
     return isLoggedIn ? (
         <>
          <div>
---
+++ routing-demo/src/app/f1/(.)f2/page.tsx
@@ -0,0 +1,3 @@
+export default function interceptedF2(){
+    return <h1>(.) intercepted F2 page</h1>
+}
